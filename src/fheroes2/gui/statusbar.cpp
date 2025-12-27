a8ed761f71bd7624588cbb3be834a525584cd03c
Blaindbro <akimvlg@gmail.com>
2025-12-27 14:06:18 +0300

    Update statusbar.cpp

--- src/fheroes2/gui/statusbar.cpp
+++ src/fheroes2/gui/statusbar.cpp
@@ -1,24 +1,24 @@
 /***************************************************************************
- *   fheroes2: https://github.com/ihhub/fheroes2                           *
- *   Copyright (C) 2019 - 2025                                             *
- *                                                                         *
- *   Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
- *   Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ * fheroes2: https://github.com/ihhub/fheroes2                           *
+ * Copyright (C) 2019 - 2025                                             *
+ * *
+ * Free Heroes2 Engine: http://sourceforge.net/projects/fheroes2         *
+ * Copyright (C) 2009 by Andrey Afletdinov <fheroes2@gmail.com>          *
+ * *
+ * This program is free software; you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation; either version 2 of the License, or     *
+ * (at your option) any later version.                                   *
+ * *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ * *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program; if not, write to the                         *
+ * Free Software Foundation, Inc.,                                       *
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
 #include "statusbar.h"
@@ -26,6 +26,10 @@
 #include <cstdint>
 #include <memory>
 #include <utility>
+
+// --- ACCESSIBILITY INCLUDE START ---
+#include "../../engine/tools.h"
+// --- ACCESSIBILITY INCLUDE END ---
 
 #include "math_tools.h"
 #include "screen.h"
@@ -43,6 +47,13 @@ fheroes2::Rect StatusBar::updateMessage( std::string msg )
         // No updates.
         return {};
     }
+
+    // --- ACCESSIBILITY CODE START ---
+    // Если сообщение изменилось и оно не пустое — озвучиваем его
+    if ( !msg.empty() ) {
+        SpeakAccessibility( msg );
+    }
+    // --- ACCESSIBILITY CODE END ---
 
     _prevMessage = msg;
 
